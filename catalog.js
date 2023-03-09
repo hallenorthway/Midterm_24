c00b95b12324a6a802dfa7677fa283efe840bc90
nnisharaj <nisharaj@iastat.edu>
2023-03-09 15:31:17 -0600

    added new files to display catalog

--- catalog.js
+++ catalog.js
@@ -1,31 +1,20 @@
-fetch("data.json")
-.then(response => response.json())
-.then(data => dataToHTML(data));
+fetch("products.json")
+.then(function(response){
+	return response.json();
+})
+.then(function(products){
+	let placeholder = document.querySelector("#data-output");
+	let out = "";
+	for(let product of products){
+		out += `
+			<tr>
+				<td> <img src='${product.url}'> </td>
+				<td>${product.productName}</td>
+				<td>${product.priceList}</td>
+				<td>${product.brand}</td>
+			</tr>
+		`;
+	}
 
-function dataToHTML(data){
-    let mainContainer = document.getElementsByClassName("card-text");
-    for (let i = 0; i < data.Tops.length; i++) {
-    let itemName = data.Tops[i].productName;
-    //let lastName = data.superheroe[i].lastName;
-    //let job = data.superheroe[i].job;
-    let mytr = document.createElement("p");
-    mytr.innerHTML = `
-  
-    <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">${itemName}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
-              </div>
-            </div>
-          </div>
-        </div> `;
-    mainContainer.appendChild(mytr);
-    }
-}
+	placeholder.innerHTML = out;
+});
