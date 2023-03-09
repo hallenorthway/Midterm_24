b589ff33e519e9360a24bfd9e0bb7e2b87c11f03
nnisharaj <nisharaj@iastat.edu>
2023-03-09 15:42:46 -0600

    added new files to display catalog

---
+++ pants.js
@@ -0,0 +1,20 @@
+fetch("pants.json")
+.then(function(response){
+	return response.json();
+})
+.then(function(products){
+	let placeholder = document.querySelector("#data-output");
+	let out = "";
+	for(let product of products){
+		out += `
+			<tr>
+				<td> <img src='${product.url}'> </td>
+				<td>${product.productName}</td>
+				<td>${product.priceList}</td>
+				<td>${product.brand}</td>
+			</tr>
+		`;
+	}
+
+	placeholder.innerHTML = out;
+});
